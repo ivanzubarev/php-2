67414be9eb43e1405ec0beb9282cfa803a5878f1
Ivan Zubarev <contact@ivanzubarev.ru>
2020-07-09 19:26:56 +0200

    Removed debug code in index.php

--- index.php
+++ index.php
@@ -5,9 +5,6 @@ use Models\Article;
 include __DIR__ . '/autoload.php';
 
 $view = new View();
-$art = Article::findById(29);
-var_dump($art->author);
-die;
 $view->lastArticles = Article::getLastArticles();
 
 $view->display(__DIR__ . '/Views/index.php');
